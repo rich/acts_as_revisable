3553d8817f87a7ad53eae3f2932638211a99b91f
Rich Cavanaugh <cavanaugh@fatjam.com>
2008-05-08 19:25:28 -0400

    Add support for query-able options to the Options class. Spec out the Options class.

--- lib/acts_as_revisable/options.rb
+++ lib/acts_as_revisable/options.rb
@@ -1,12 +1,13 @@
 module FatJam
   module ActsAsRevisable
-    class Options
-      def initialize(options, &block)
-        @options = options
+    class Options      
+      def initialize(*options, &block)
+        @options = options.extract_options!
         instance_eval(&block) if block_given?
       end
 
       def method_missing(key, *args)
+        return (@options[key.to_s.gsub(/\?$/, '').to_sym].eql?(true)) if key.to_s.match(/\?$/)
         if args.blank?
           @options[key.to_sym]
         else
---
+++ spec/aar_options_spec.rb
@@ -0,0 +1,83 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+shared_examples_for "common Options usage" do
+  it "should return a set value" do
+    @options.one.should == 1
+  end
+  
+  it "should return nil for an unset value" do
+    @options.two.should be_nil
+  end
+  
+  it "should return false for unset query option" do
+    @options.unset?.should be_false
+  end
+  
+  it "should return true for a query option set to true" do
+    @options.yes?.should be_true
+  end
+  
+  it "should return false for a query option set to false" do
+    @options.no?.should be_false
+  end
+  
+  it "should return false for a query on a non-boolean value" do
+    @options.one?.should be_false
+  end
+  
+  it "should return an array when passed one" do
+    @options.arr.should be_a_kind_of(Array)
+  end
+  
+  it "should not return an array when not passed one" do
+    @options.one.should_not be_a_kind_of(Array)
+  end
+  
+  it "should have the right number of elements in an array" do
+    @options.arr.size.should == 3
+  end
+end
+
+describe FatJam::ActsAsRevisable::Options do
+  describe "with hash options" do
+    before(:each) do
+      @options = FatJam::ActsAsRevisable::Options.new :one => 1, :yes => true, :no => false, :arr => [1,2,3]
+    end
+    
+    it_should_behave_like "common Options usage"
+  end
+  
+  describe "with block options" do
+    before(:each) do
+      @options = FatJam::ActsAsRevisable::Options.new do
+        one 1
+        yes true
+        arr [1,2,3]
+      end
+    end
+    
+    it_should_behave_like "common Options usage"
+  end
+  
+  describe "with both block and hash options" do
+    before(:each) do
+      @options = FatJam::ActsAsRevisable::Options.new(:yes => true, :arr => [1,2,3]) do
+        one 1
+      end
+    end
+    
+    it_should_behave_like "common Options usage"
+    
+    describe "the block should override the hash" do
+      before(:each) do
+        @options = FatJam::ActsAsRevisable::Options.new(:yes => false, :one => 10, :arr => [1,2,3,4,5]) do
+          one 1
+          yes true
+          arr [1,2,3]
+        end
+      end
+      
+      it_should_behave_like "common Options usage"      
+    end
+  end
+end
