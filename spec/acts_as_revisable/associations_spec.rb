0b321f92e3a6f00a1e9618dafa9ed3665d263c4d
Shay Frendt <shay.frendt@gmail.com>
2012-07-09 15:16:49 -0400

    with_revisions is not a valid key for associations
    - This is a failing test to show that this behavior does not work as
      expected on Rails 3. The override of the find method in revisable.rb
      no longer works because of the way Rails 3 does collection proxies now
      whereas it used to just do collection associations

--- spec/acts_as_revisable/associations_spec.rb
+++ spec/acts_as_revisable/associations_spec.rb
@@ -19,4 +19,9 @@ describe WithoutScope::ActsAsRevisable do
   it "should return all the revisions" do
     @project.revisions.size.should == 3
   end
+
+  it "should allow :with_revisions key for associations" do
+    Person.create(:name => "Peter", :project => @project)
+    lambda { @project.people.find(:first, :with_revisions => true) }.should_not raise_error
+  end
 end
